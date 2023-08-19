58c614b9bb7305f1c2e5954c58052a91971e1418
Jia-Jia Zhu <dodge_waking_0b@icloud.com>
2023-08-19 00:02:24 -0700

    finish lab

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Article() {
-  return (
-    <div>
-      {/* please pass this test */}
-    </div>
-  );
+  return <div>please pass this test</div>;
 }
 
 export default Article;
